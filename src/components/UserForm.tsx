a7978b1626d133879c8054a0c12bb4ef7ed024b9
Akanksha2596 <akanksha.yadav@brainvire.com>
2024-02-15 18:14:43 +0530

    validation and new form elements.

--- src/components/UserForm.tsx
+++ src/components/UserForm.tsx
@@ -12,19 +12,62 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import { toast } from "@/components/ui/use-toast";
 import { Input } from "@/components/ui/input";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { RadioGroup, RadioGroupItem } from "./ui/radio-group";
+import { Calendar } from "@/components/ui/calendar";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+import { CalendarIcon } from "@radix-ui/react-icons";
+import { cn } from "@/lib/utils";
+import { format } from "date-fns";
 
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-  emailAddress: z.string().email(), 
-  accountType: z.enum(["personal", "company"]),
-  companyName: z.string().optional(),
-  password: z.string().min(5),    
-  passwordConfirm: z.string(),  
-})
+const formSchema = z
+  .object({
+    username: z.string().min(2, {
+      message: "Username must be at least 2 characters.",
+    }),
+    emailAddress: z.string().email(),
+    accountType: z.enum(["personal", "company"]),
+    companyName: z.string().optional(),
+    password: z.string().min(5, {
+      message: "Password must be at least 5 characters.",
+    }),
+    passwordConfirm: z.string(),
+    type: z.enum(["all", "mentions", "none"], {
+      required_error: "You need to select a notification type.",
+    }),
+    dob: z.date({
+      required_error: "A date of birth is required.",
+    }),
+  })
+  .refine(
+    (data) => {
+      return data.password === data.passwordConfirm;
+    },
+    {
+      message: "Passwords do not match",
+      path: ["passwordConfirm"],
+    }
+  )
+  .refine(
+    (data) => {
+      if (data.accountType === "company") {
+        return !!data.companyName;
+      }
+      return true;
+    },
+    {
+      message: "Company name is required",
+      path: ["companyName"],
+    }
+  );
 
 export default function ProfileForm() {
   //Define your form.
@@ -32,19 +75,27 @@ export default function ProfileForm() {
     resolver: zodResolver(formSchema),
     defaultValues: {
       username: "",
-      emailAddress:"",
+      emailAddress: "",
       companyName: "",
-      password:"",
-      passwordConfirm:"",
+      password: "",
+      passwordConfirm: "",
     },
-  })
+  });
   const accountType = form.watch("accountType");
-  
+
   //Define a submit handler.
   function onSubmit(values: z.infer<typeof formSchema>) {
     // Do something with the form values.
     //  This will be type-safe and validated.
-    console.log(values)
+    toast({
+      title: "You submitted the following values:",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(values, null, 2)}</code>
+        </pre>
+      ),
+    });
+    console.log(values);
   }
 
   return (
@@ -75,53 +126,52 @@ export default function ProfileForm() {
               <FormControl>
                 <Input placeholder="abc@gmail.com" {...field} />
               </FormControl>
-              <FormDescription>
-                This is your Email.
-              </FormDescription>
+              <FormDescription>This is your Email.</FormDescription>
+
               <FormMessage />
             </FormItem>
           )}
         />
         <FormField
+          control={form.control}
+          name="accountType"
+          render={({ field }) => {
+            return (
+              <FormItem>
+                <FormLabel>Account type</FormLabel>
+                <Select onValueChange={field.onChange}>
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select an account type" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectItem value="personal">Personal</SelectItem>
+                    <SelectItem value="company">Company</SelectItem>
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            );
+          }}
+        />
+        {accountType === "company" && (
+          <FormField
             control={form.control}
-            name="accountType"
+            name="companyName"
             render={({ field }) => {
               return (
                 <FormItem>
-                  <FormLabel>Account type</FormLabel>
-                  <Select onValueChange={field.onChange}>
-                    <FormControl>
-                      <SelectTrigger>
-                        <SelectValue placeholder="Select an account type" />
-                      </SelectTrigger>
-                    </FormControl>
-                    <SelectContent>
-                      <SelectItem value="personal">Personal</SelectItem>
-                      <SelectItem value="company">Company</SelectItem>
-                    </SelectContent>
-                  </Select>
+                  <FormLabel>Company name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Company name" {...field} />
+                  </FormControl>
                   <FormMessage />
                 </FormItem>
               );
             }}
           />
-           {accountType === "company" && (
-            <FormField
-              control={form.control}
-              name="companyName"
-              render={({ field }) => {
-                return (
-                  <FormItem>
-                    <FormLabel>Company name</FormLabel>
-                    <FormControl>
-                      <Input placeholder="Company name" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                );
-              }}
-            />
-          )}
+        )}
         <FormField
           control={form.control}
           name="password"
@@ -129,32 +179,119 @@ export default function ProfileForm() {
             <FormItem>
               <FormLabel>Password</FormLabel>
               <FormControl>
-                <Input placeholder="password here" {...field} />
+                <Input placeholder="password here" type="password" {...field} />
+              </FormControl>
+              <FormDescription>Your password confidential.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="passwordConfirm"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Confirm Password</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="confirm password"
+                  type="password"
+                  {...field}
+                />
               </FormControl>
               <FormDescription>
-                Your password confidential.
+                Should be same as your password.
               </FormDescription>
               <FormMessage />
             </FormItem>
           )}
-        /><FormField
-        control={form.control}
-        name="passwordConfirm"
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel>Confirm Password</FormLabel>
-            <FormControl>
-              <Input placeholder="confirm password" {...field} />
-            </FormControl>
-            <FormDescription>
-              Should be same as your password.
-            </FormDescription>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
+        />
+        <FormField
+          control={form.control}
+          name="dob"
+          render={({ field }) => (
+            <FormItem className="flex flex-col">
+              <FormLabel>Date of birth</FormLabel>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button
+                      variant={"outline"}
+                      className={cn(
+                        "w-[240px] pl-3 text-left font-normal",
+                        !field.value && "text-muted-foreground"
+                      )}
+                    >
+                      {field.value ? (
+                        format(field.value, "PPP")
+                      ) : (
+                        <span>Pick a date</span>
+                      )}
+                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0" align="start">
+                  <Calendar
+                    mode="single"
+                    selected={field.value}
+                    onSelect={field.onChange}
+                    disabled={(date) =>
+                      date > new Date() || date < new Date("1900-01-01")
+                    }
+                    initialFocus
+                  />
+                </PopoverContent>
+              </Popover>
+              <FormDescription>
+                Your date of birth is used to calculate your age.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="type"
+          render={({ field }) => (
+            <FormItem className="space-y-3">
+              <FormLabel>Notify me about...</FormLabel>
+              <FormControl>
+                <RadioGroup
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                  className="flex flex-col space-y-1"
+                >
+                  <FormItem className="flex items-center space-x-3 space-y-0">
+                    <FormControl>
+                      <RadioGroupItem value="all" />
+                    </FormControl>
+                    <FormLabel className="font-normal">
+                      All new messages
+                    </FormLabel>
+                  </FormItem>
+                  <FormItem className="flex items-center space-x-3 space-y-0">
+                    <FormControl>
+                      <RadioGroupItem value="mentions" />
+                    </FormControl>
+                    <FormLabel className="font-normal">
+                      Direct messages and mentions
+                    </FormLabel>
+                  </FormItem>
+                  <FormItem className="flex items-center space-x-3 space-y-0">
+                    <FormControl>
+                      <RadioGroupItem value="none" />
+                    </FormControl>
+                    <FormLabel className="font-normal">Nothing</FormLabel>
+                  </FormItem>
+                </RadioGroup>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
         <Button type="submit">Submit</Button>
       </form>
     </Form>
-  )
+  );
 }
