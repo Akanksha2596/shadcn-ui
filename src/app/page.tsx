937e6479bb539b7ec1786d745c738e7b35074653
Akanksha2596 <akanksha.yadav@brainvire.com>
2024-02-16 19:43:31 +0530

    adding custom styles.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,12 @@ export default function Home() {
       <Nav />
       <section className="py-12 flex flex-col items-center text-center gap-8">
         <h1 className="text-4xl font-bold">Shadcn is awesome</h1>
-        <p className="text-2xl text-muted-foreground">Hand-picked themes that you can copy and paste into your apps.</p>
+        {/* <p className="text-2xl text-muted-foreground">Hand-picked themes that you can copy and paste into your apps.</p> */}
+        <p className="text-2xl text-foreground">Hand-picked themes that you can copy and paste into your apps.</p>
       </section>
       <div className="flex items-center justify-center gap-6 ">
-        <Button variant={"secondary"}>Learn More</Button>
+        {/* <Button variant={"secondary"}>Learn More</Button> */}
+        <Button variant={"custom"}>Learn More</Button>
         <Button>Enroll Now</Button>
       </div>
       <ProfileForm/>
